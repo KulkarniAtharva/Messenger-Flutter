74b1a9d94a15b87eea03162a5f858fc2b4c6832c
Atharva Kulkarni <atharvakulkarni2204@gmail.com>
2021-02-18 01:43:24 +0530

    added shared preferences

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:messenger_flutter/data/constants.dart';
 import 'package:messenger_flutter/models/user_is_model.dart';
 
 class AuthService
